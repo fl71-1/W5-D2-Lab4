f3c839ccbc60256c0d3f0863d8f81a17cc789751
fahd alghtni <fahdalghtni246@gmail.com>
2024-09-15 10:40:13 +0300

    lab4

---
+++ min.js
@@ -0,0 +1,29 @@
+const BOX = document.getElementById('box');
+const widthI = document.getElementById('boxWidth');
+const hieghtI = document.getElementById('boxHieght');
+const colorI = document.getElementById('boxColor');
+const raduisI = document.getElementById('boxReduis');
+const reset = document.getElementById('reset');
+
+widthI.addEventListener('change', function() {
+    BOX.style.width = widthI.value + "vw";
+});
+
+hieghtI.addEventListener('change', function() {
+    BOX.style.height = hieghtI.value + "vh";
+});
+
+raduisI.addEventListener('change', function() {
+    BOX.style.borderRadius = raduisI.value + "px";
+});
+
+colorI.addEventListener('change', function() {
+    BOX.style.backgroundColor = colorI.value;
+});
+
+reset.addEventListener('click', function() {
+    BOX.style.width = "30vw";
+    BOX.style.height = "40vh";
+    BOX.style.backgroundColor = "blue";
+    BOX.style.borderRadius = "0px";
+});
